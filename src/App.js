ab4b7b649ca3e78c5e7c01edf563d95243f34a0f
Phil G <fillupgalbraith@gmail.com>
2021-11-05 12:28:18 -0700

    corrected the styles kept data in array

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import styled from 'styled-components';
-import Character from './components/Character'
+import Character from './components/Character';
 
 const StyledApp = styled.div`
   .App {
@@ -11,8 +11,6 @@ const StyledApp = styled.div`
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-start;
-    background-color: slategray;
-    border: 2px lightgray;
     padding: 0vw;
     margin: 5vw;
     box-sizing: border-box;
@@ -23,25 +21,24 @@ const StyledApp = styled.div`
     font-family: 'Tahoma', sans-serif;
     font-weight: 600;
     font-size: 4rem;
-    color: green;
+    color: black;
   }
 
   @media (max-width: 1200px) {
     .Header {
       font-size: 4rem;
-      color: #00FF44;
+      color: #FF4400;
     }
   }
 
   @media (max-width: 900px) {
     .App {
-      background-color: rgba( 0,0,0,0.5);
       margin: 2.5vw;
       border: 2px #777777;
     }
     .Header {
       font-size: 3.4rem;
-      color: #FF8844;
+      color: #AA4400;
     }
   }
 
@@ -49,16 +46,19 @@ const StyledApp = styled.div`
     .App {
       margin: 1vw;
       border: 2px #555555;
+      width: auto;
     }
 
     .Header {
+      width: auto;
       font-size: 3rem;
-      color: #FFBB88;
+      color: #444444;
     }
   }
 
   @media (max-width: 320px) {
     .App {
+      width: auto;
       margin: 0.5vw;
       border: 1px #444444;
     }
@@ -72,7 +72,7 @@ const StyledApp = styled.div`
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [swData, setSwData] = useState([])
+  const [swData, setSwData] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -85,15 +85,18 @@ const App = () => {
       .catch( err => console.error(err))
     }, []
   ) 
+  console.log(swData);
   return (
     <StyledApp className='App'>
+      <div>
       <h1 className='Header'>CHARACTERS</h1>
-      {swData.forEach(character => {
-        return (<Character 
-        characterName={character.name} 
-        birthYear={character.birth_year}
-        />)
-      })}
+      {swData.map( character => {
+        return (<Character
+          characterName={character[0]}
+          birthYear={character[1]}
+        />)}
+      )}
+      </div>
     </StyledApp>
   )
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,72 +1,95 @@
 // Write your Character component here
 import React from 'react';
-//import styled from 'styled-components';
-/*
+import styled from 'styled-components';
+
 const StyledCharacter = styled.span`
-  .Character {
-    border: 2px #66AA00;
-    background-color: none;
-    margin: 0px;
+
+  .containerDiv {
+    width: 80vw;
+    margin: 0 10vw;
+    font-size: 3rem;
   }
 
-  .div {
+  .rowDiv {
+    padding: 0 5vw;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    text-align: start;
-    line-height: 1.5rem;
-    text-decoration: 2px underline gray, 1px overline yellow;
+    line-height: 1.5;
     background-color: rgba( 0,0,0,0.2);
     color: white;
     margin: 0px;
-    font-family: 'Times New Roman', serif;
+    border: 2px solid #AA4411;
+    font-family: 'Arial', serif;
+  }
+
+  .wordDiv{
+    background-color: rgba( 0,0,0,0.2);
+    border-radius: 3px;
+    box-shadow: 0 5px 5px rgba( 0,0,0,0.2),0 -5px 5px rgba( 0,0,0,0.2);
   }
 
   @media (max-width: 1200px) {
-    .div {
-      line-height: 1.3rem;
-      text-decoration: 1px underline darkgray, 1px underline lightgray;
+    .rowDiv {
+      line-height: 1.3;
       font-family: 'Didot', serif;
     }
   }
 
   @media (max-width: 900px) {
-    .div {
-      color: lightskyblue;
-      line-height: 1.2rem;
-      text-decoration: 1px underline black, 1px overline lightgray;
-      font-family: 'Impact', sans-serif;
+    
+    .rowDiv {
+      padding: 0 3vw;
+      color: #DDEEFF;
+      line-height: 1.2;
+      font-family: 'Arial', sans-serif;
+    }
+    .containerDiv {
+      font-size: 2.2rem;
     }
   }
+
     @media (max-width: 500px) {
-    .div {
-      line-height: 1rem;
+    .rowDiv {
+      padding: 0 3vw; 
+      line-height: 1;
       color: lightgoldenrodyellow;
-      text-decoration: 1px underline darkorange, 1px overline lightgray;
       background-color: rgba( 0,0,0,0.3);;
       font-family: 'Tahoma', sans-serif;
+      width: auto;
+      padding: 1vw 0;
+    }
+    .containerDiv {
+        width: auto;
+      font-size: 1.5rem;
     }
   }
+
   @media (max-width: 320px) {
-    .div {
+    .rowDiv {
+      width: auto;
       color: white;
       background-color: rgba( 0,0,0,0.7);;
-      border: 1px #444444;
+      border: 1px solid #444444;
       text-decoration: none;
       font-family: 'Arial', sans-serif;
+      padding: 1vw 0;
     }
   }
 `
-*/
+
 const Character = ({characterName, birthYear}) => {
-    
-    console.log("Character");
     return (
-        <div className='Character' key={characterName}>
-            <div>{characterName} {birthYear}</div> 
+      <StyledCharacter>
+        <div className='containerDiv'>
+          <div className='rowDiv'>
+            <div className="wordDiv">{characterName}</div>
+            <div className="wordDiv">{birthYear}</div>
+          </div>
         </div>
+      </StyledCharacter>
     )
 }
 
-export default Character;
+export default Character
