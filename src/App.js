e8a806e199c89df867c647705977ec42195e1281
Phil G <fillupgalbraith@gmail.com>
2021-11-05 11:02:27 -0700

    correct data stored in swData

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from "axios";
+import axios from 'axios';
 import styled from 'styled-components';
+import Character from './components/Character'
 
 const StyledApp = styled.div`
   .App {
@@ -10,33 +11,22 @@ const StyledApp = styled.div`
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-start;
-    text-align: start;
-    line-height: 1.5rem;
-    text-decoration: 2px underline black, 2px underline black;
     background-color: slategray;
-    color: palegreen;
     border: 2px lightgray;
+    padding: 0vw;
     margin: 5vw;
-    padding: 1vw;
     box-sizing: border-box;
     border-radius: 2px;
-    font-family: "Times New Roman", serif;
   }
 
  .Header {
-    font-family: "Tahoma", sans-serif;
+    font-family: 'Tahoma', sans-serif;
     font-weight: 600;
     font-size: 4rem;
     color: green;
   }
 
   @media (max-width: 1200px) {
-    .App {
-      font-size: medium;
-      line-height: 1.4rem;
-      text-decoration: 1px underline black, 1px underline black;
-      font-family: "Didot", serif;
-    }
     .Header {
       font-size: 4rem;
       color: #00FF44;
@@ -45,14 +35,9 @@ const StyledApp = styled.div`
 
   @media (max-width: 900px) {
     .App {
-      text-decoration: none;
-      line-height: 1.2rem;
-      color: darkslateblue;
       background-color: rgba( 0,0,0,0.5);
-      padding: 0.5vw;
       margin: 2.5vw;
       border: 2px #777777;
-      font-family: "Impact", sans-serif;
     }
     .Header {
       font-size: 3.4rem;
@@ -62,14 +47,10 @@ const StyledApp = styled.div`
 
   @media (max-width: 500px) {
     .App {
-      line-height: 1.2rem;
-      color: darkorange;
-      background-color: rgba( 0,0,0,0.6);;
-      padding: 0vw;
       margin: 1vw;
       border: 2px #555555;
-      font-family: "Tahoma", sans-serif;
     }
+
     .Header {
       font-size: 3rem;
       color: #FFBB88;
@@ -78,13 +59,8 @@ const StyledApp = styled.div`
 
   @media (max-width: 320px) {
     .App {
-      line-height: 1.2rem;
-      color: green;
-      background-color: rgba( 0,0,0,0.7);;
-      padding: 0vw;
       margin: 0.5vw;
       border: 1px #444444;
-      font-family: "Arial", sans-serif;
     }
     .Header {
       font-size: 2rem;
@@ -105,16 +81,21 @@ const App = () => {
       .then( resp => {
         setSwData( resp.data.map( character => [character.name, character.birth_year])
         );
-        console.log(swData);
       })
       .catch( err => console.error(err))
     }, []
   ) 
   return (
-    <StyledApp className="App">
-      <h1 className="Header">CHARACTERS</h1>
+    <StyledApp className='App'>
+      <h1 className='Header'>CHARACTERS</h1>
+      {swData.forEach(character => {
+        return (<Character 
+        characterName={character.name} 
+        birthYear={character.birth_year}
+        />)
+      })}
     </StyledApp>
-  );
+  )
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,72 @@
 // Write your Character component here
+import React from 'react';
+//import styled from 'styled-components';
+/*
+const StyledCharacter = styled.span`
+  .Character {
+    border: 2px #66AA00;
+    background-color: none;
+    margin: 0px;
+  }
+
+  .div {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    text-align: start;
+    line-height: 1.5rem;
+    text-decoration: 2px underline gray, 1px overline yellow;
+    background-color: rgba( 0,0,0,0.2);
+    color: white;
+    margin: 0px;
+    font-family: 'Times New Roman', serif;
+  }
+
+  @media (max-width: 1200px) {
+    .div {
+      line-height: 1.3rem;
+      text-decoration: 1px underline darkgray, 1px underline lightgray;
+      font-family: 'Didot', serif;
+    }
+  }
+
+  @media (max-width: 900px) {
+    .div {
+      color: lightskyblue;
+      line-height: 1.2rem;
+      text-decoration: 1px underline black, 1px overline lightgray;
+      font-family: 'Impact', sans-serif;
+    }
+  }
+    @media (max-width: 500px) {
+    .div {
+      line-height: 1rem;
+      color: lightgoldenrodyellow;
+      text-decoration: 1px underline darkorange, 1px overline lightgray;
+      background-color: rgba( 0,0,0,0.3);;
+      font-family: 'Tahoma', sans-serif;
+    }
+  }
+  @media (max-width: 320px) {
+    .div {
+      color: white;
+      background-color: rgba( 0,0,0,0.7);;
+      border: 1px #444444;
+      text-decoration: none;
+      font-family: 'Arial', sans-serif;
+    }
+  }
+`
+*/
+const Character = ({characterName, birthYear}) => {
+    
+    console.log("Character");
+    return (
+        <div className='Character' key={characterName}>
+            <div>{characterName} {birthYear}</div> 
+        </div>
+    )
+}
+
+export default Character;
